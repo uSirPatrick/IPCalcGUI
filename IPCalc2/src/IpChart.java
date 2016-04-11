f02ed0e4f751dd627c530f76a000ff826b49ee35
jonathan <jonathan@ubuntu>
2016-04-11 16:11:08 -0700

    Almost finished table.

--- IPCalc2/src/IpChart.java
+++ IPCalc2/src/IpChart.java
@@ -1,42 +1,119 @@
-import java.util.Arrays;
-
-
-public class IpChart{
+public class IpChart
+{
 	
-	static char[][] table = new char[32][64];
-
-	public static void IpChart(){
-		
-		for(char[] row : table)
+	
+	public static void main(String[] args) 
+	{
+	int baseNumber = 2;
+	int bitNumber = 1;
+	int f = 1;
+	int l = 1;
+	System.out.print(" ");
+	while(l <= 32)
+	{
+		System.out.print("_");
+		l++;
+	}
+	System.out.println("");
+	System.out.print("|");
+	while(f <= 12)
+	{
+		System.out.print(" ");
+		f++;
+	}
+	System.out.print("|");
+	while(f <= 31)
+	{
+		System.out.print(" ");
+		f++;
+	}
+	System.out.println("|");
+	while(bitNumber <= 32)
+	{
+		long result = (long) Math.pow(baseNumber, bitNumber);
+		int i = 1;
+		int x = 1;
+		int z = 1;
+		System.out.print("|");
+		while(x <= 4)
+		    {
+				System.out.print(" ");
+				x++;
+		    }
+		System.out.print(bitNumber);
+		if(bitNumber >= 10)
 		{
-			Arrays.fill(row,'*');
-			
+			while(i <= 6)
+			{
+				System.out.print(" ");
+				i++;
+			}
+			System.out.print("|");
+			while(i <= 14)
+			{
+				System.out.print(" ");
+				i++;
+			}
+		}
+		if(bitNumber <=9)
+		{
+			while(i <= 7)
+			{
+				System.out.print(" ");
+				i++;
+			}
+			System.out.print("|");
+			while(i <= 15)
+			{
+				System.out.print(" ");
+				i++;
+			}
+		}
+		System.out.println(result);
+		bitNumber++;
+		System.out.print("|");
+		while(z <= 12)
+			{
+			System.out.print("_");
+			z++;
+			}
+		System.out.print("|");
+		while(z <= 31)
+			{
+			System.out.print("_");
+			z++;
+			}
+		System.out.println("|");
+		if(bitNumber <= 32)
+		{
+			System.out.print("|");
+			int y = 1;
+			while(y <= 12)
+			{
+				System.out.print(" ");
+				y++;
+			}
+			System.out.print("|");
+			while(y <= 31)
+			{
+				System.out.print(" ");
+				y++;
+			}
+			System.out.println("|");
 		}
 	}
-	public static void  drawTable()
-	{
-		int a = 1;
-		while(a < 60){System.out.print('_'); a++; }
-		System.out.println();
-		
-		int bits = 1;
-	    int number = (int)Math.pow(2, bits);
-		while(bits < 33){System.out.print("| ");
-			System.out.println(" "  + bits); bits++; }
-		System.out.println();
-				
-				
 	}
 
-	}
-				
+
+		
+
+}
 
 	
 		
 		
 
 	
-
 
 
 
